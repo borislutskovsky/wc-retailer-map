1c08b812a7cf6c750818a479d2c171a7db14d060
Boris Lutskovsky <borislutskovsky@gmail.com>
2017-03-06 12:26:56 -0800

    initial import

---
+++ js/wc-retailers-map-admin.js
@@ -0,0 +1,12 @@
+(function(){
+  "use strict";
+
+  angular.module('wcRetailersMap', [])
+    .controller('wcRetailersMapController', wcRetailersMapController);
+
+  wcRetailersMapController.$inject = [];
+
+  function wcRetailersMapController(){
+    var vm = this;
+  }
+})();
--- wc-retailer-map.php
+++ wc-retailer-map.php
@@ -32,11 +32,21 @@ function wc_retailers_register_post_type(){
     'menu_icon' => 'dashicons-location-alt',
     'register_meta_box_cb' => 'wc_retailer_post_additional_fields'
   ));
+
 }
 
 function wc_retailer_post_additional_fields($post){
+
+
   add_meta_box('wc_retailer_map_address', 'Physical Address', 'wc_retailer_metabox_address_cb', 'retailer', 'advanced', 'high');
   add_meta_box('wc_retailer_map_contact', 'Contact Info', 'wc_retailer_metabox_contact_cb', 'retailer', 'advanced', 'high');
+
+  wp_enqueue_script('angular', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.2/angular.min.js');
+
+  wp_enqueue_script( 'wc_retailers_map', plugin_dir_url(__FILE__).'js/wc-retailers-map-admin.js', true );
+  wp_enqueue_style('wc_retailers_map', plugin_dir_url(__FILE__).'css/wc-retailers-map-admin.css', true);
+
+
 }
 
 function wc_retailer_metabox_contact_cb($post, $metabox){
@@ -60,17 +70,27 @@ function wc_retailer_metabox_address_cb($post, $metabox){
   $state = get_post_meta($post->ID, 'wc_retailer_map_state', true);
   $country = get_post_meta($post->ID, 'wc_retailer_map_country', true);
   $postal_code = get_post_meta($post->ID, 'wc_retailer_map_postal', true);
+  $country = get_post_meta($post->ID, 'wc_retailers_map_country', true);
+  $lat = get_post_meta($post->ID, 'wc_retailers_map_lat', true);
+  $long = get_post_meta($post->ID, 'wc_retailers_map_long', true);
 
   ?>
-  <p class="post-attributes-label-wrapper"><label for="wc_retailer_map_address" class="post-attributes-label"><?php echo  __('Address'); ?></label></p>
-  <input name="wc_retailer_map_address" value="<?php echo $address; ?>" />
-  <br/>
-  <p class="post-attributes-label-wrapper"><label for="wc_retailer_map_city" class="post-attributes-label"><?php echo __('City'); ?></label></p>
-  <input name="wc_retailer_map_city" value="<?php echo $city; ?>" />
-  <br/>
-  <p class="post-attributes-label-wrapper"><label for="wc_retailer_map_state" class="post-attributes-label"><?php echo __('State'); ?></label></p>
-  <input name="wc_retailer_map_state" value="<?php echo $state; ?>" />
-
+  <div ng-app="wcRetailersMap" ng-controller="wcRetailersMapController">
+    <p class="post-attributes-label-wrapper"><label for="wc_retailer_map_address" class="post-attributes-label"><?php echo  __('Address'); ?></label></p>
+    <input name="wc_retailer_map_address" value="<?php echo $address; ?>" />
+    <br/>
+    <p class="post-attributes-label-wrapper"><label for="wc_retailer_map_city" class="post-attributes-label"><?php echo __('City'); ?></label></p>
+    <input name="wc_retailer_map_city" value="<?php echo $city; ?>" />
+    <br/>
+    <p class="post-attributes-label-wrapper"><label for="wc_retailer_map_state" class="post-attributes-label"><?php echo __('State'); ?></label></p>
+    <input name="wc_retailer_map_state" value="<?php echo $state; ?>" />
+    <p class="post-attributes-label-wrapper"><label for="wc_retailers_map_postal" class="post-attributes-label"><?php echo __('Postal Code'); ?></label></p>
+    <input name="wc_retailers_map_postal" value="<?php echo $postal_code; ?>" />
+    <p class="post-attributes-label-wrapper"><label for="wc_retailers_map_country" class="post-attributes-label"><?php echo __('Country'); ?></label></p>
+    <input name="wc_retailers_map_country" value="<?php echo $country; ?>" />
+    <input name="wc_retailers_map_lat" value="<?php echo $lat; ?>" type="hidden" />
+    <input name="wc_retailers_map_long" value="<?php echo $long; ?>" type="hidden" />
+  </div>
   <?php
 }
 
